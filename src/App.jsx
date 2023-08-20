b08c574a95973f25f951e243e0b690f5d63e564d
glitton <glcodeworks@gmail.com>
2023-08-20 15:48:51 -0500

    fetch jobs from api

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+const url = "https://course-api.com/react-tabs-project";
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  // current Item
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+  // console.log(jobs);
+
+  if (isLoading) {
+    return (
+      <section className='jobs-center'>
+        <div className='loading'></div>
+      </section>
+    );
+  }
+  return <h2>Tabs Starter</h2>;
+};
+export default App;
